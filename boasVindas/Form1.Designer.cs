4208b1828530c93eeda0160fd64b52f4b11f7342
Safra-Man <willsafra@gmail.com>
2021-12-13 17:26:08 -0300

    Aula 01-FIAP

---
+++ boasVindas/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace boasVindas
+{
+    partial class frmAula01
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBoasVindas = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBoasVindas
+            // 
+            this.btnBoasVindas.Location = new System.Drawing.Point(224, 198);
+            this.btnBoasVindas.Name = "btnBoasVindas";
+            this.btnBoasVindas.Size = new System.Drawing.Size(112, 34);
+            this.btnBoasVindas.TabIndex = 0;
+            this.btnBoasVindas.Text = "Clique Aqui";
+            this.btnBoasVindas.UseVisualStyleBackColor = true;
+            this.btnBoasVindas.Click += new System.EventHandler(this.btnBoasVindas_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Location = new System.Drawing.Point(278, 99);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 25);
+            this.lblMensagem.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digite o seu nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(190, 63);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(373, 31);
+            this.txtNome.TabIndex = 3;
+            // 
+            // frmAula01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 455);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnBoasVindas);
+            this.Name = "frmAula01";
+            this.Text = "Aula 01";
+      
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnBoasVindas;
+        private Label lblMensagem;
+        private Label label1;
+        private TextBox txtNome;
+    }
+}
---
+++ boasVindas/Form1.cs
@@ -0,0 +1,25 @@
+namespace boasVindas
+{
+    public partial class frmAula01 : Form
+    {
+        public frmAula01()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBoasVindas_Click(object sender, EventArgs e)
+        {
+            string nomeUsuario; //Declaração de uma variável do tipo string
+
+            nomeUsuario = txtNome.Text;
+
+            //Concatenar
+
+
+            MessageBox.Show("Bem vindo ao C# " + nomeUsuario,"Olá",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            lblMensagem.Text = ($"Bem vindo ao C#! {nomeUsuario}"); // Diferentes tipos de Interpolação
+        }
+
+     
+    }
+}
